6183aa7a135cee295f3851f0fcf590f19f8fbeaf
isabela-tech <isaabgon@gmail.com>
2025-06-10 11:47:19 -0300

    listaexercicio3_resolvido.py

--- listaexercicio3_resolvido.py
+++ listaexercicio3_resolvido.py
@@ -27,9 +27,9 @@ st.set_page_config(
 )
 import pandas as pd
 
-arquivo = "/content/projetos.csv"
+arquivo = "https://raw.githubusercontent.com/isabela-tech/exercicios_lista3/refs/heads/main/projetos.csv"
 df = pd.read_csv(arquivo, sep=';')
-df.head(len(df))
+st.dataframe(df.head(len(df)))
 
 """2) Atualize a DataFrame criado no Exe1, adicionando mais uma linha ao final com os dados referentes ao mês de dezembro de 2026. Depois apresente as últimas linhas da df para checar se ocorreu como deveria. (Peso: 2,0)
 
